a387fb23956012e6b1494b55a03cfbbbf7cb55f9
Gabriel Marques <gabriel.chagas.marques@gmail.com>
2019-05-10 23:17:57 -0700

    chore(ci): setup travis

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    ['lodash', { id: ['lodash', 'recompact'] }],
+    '@babel/plugin-transform-react-jsx',
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
