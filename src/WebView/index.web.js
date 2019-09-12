46c2f80ff83f34535731db01a142bc253a063251
CareLuLu Jonathan <jonathan.github@carelulu.com>
2019-09-12 23:32:51 +1200

    feat(addWebViewForWeb): add WebView component to display HTML content for the web
    Closes #84

--- src/WebView/index.web.js
+++ src/WebView/index.web.js
@@ -1,3 +1,17 @@
-const WebView = () => null;
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Parser } from 'html-to-react';
+
+const htmlToReactParser = new Parser();
+
+const WebView = ({ source }) => (
+  <React.Fragment>
+    {htmlToReactParser.parse(source.html)}
+  </React.Fragment>
+);
+
+WebView.propTypes = {
+  source: PropTypes.shape().isRequired,
+};
 
 export default WebView;
