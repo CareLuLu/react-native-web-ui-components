3cccb95dced64c1973109ad59cad70d652c1584e
CareLuLu Gabriel <gabriel@carelulu.com>
2020-10-13 09:55:54 -0700

    fix(select): use buffer.from on nodejs env

--- src/ModalPicker/index.web.js
+++ src/ModalPicker/index.web.js
@@ -80,12 +80,27 @@ const measureTextWidth = (txt) => {
   return width;
 };
 
-const hexToRgb = (color) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
-  if (result) {
-    return `rgb(${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)})`;
+const toBase64 = (text) => {
+  try {
+    return btoa(text);
+  } catch (err) {
+    return Buffer.from(text).toString('base64');
   }
-  return color || 'rgb(0,0,0)';
+};
+
+const hexToRgb = (color) => {
+  try {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color || '');
+    if (result) {
+      return `rgb(${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)})`;
+    }
+    if (/^rgb/.test(color || '')) {
+      return color;
+    }
+    return 'rgb(0,0,0)';
+  } catch (err) {
+    return 'rgb(0,0,0)';
+  }
 };
 
 const Input = ({
@@ -148,7 +163,7 @@ const Input = ({
             }
             ${arrow ? `
               [data-class~="${id.current}__container"] input {
-                background-image: url('data:image/svg+xml;base64,${btoa(`<svg xmlns="http://www.w3.org/2000/svg" fill="${hexToRgb(color)}" height="20" width="20" viewBox="0 0 20 20"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z" /></svg>`)}');
+                background-image: url('data:image/svg+xml;base64,${toBase64(`<svg xmlns="http://www.w3.org/2000/svg" fill="${hexToRgb(color)}" height="20" width="20" viewBox="0 0 20 20"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z" /></svg>`)}');
                 background-repeat: no-repeat;
                 background-position: right 4px center;
               }
